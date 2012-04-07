6ccd9eb712fc3170ee4f0292e3297e1cf60aaaac
Miloš Hadžić <milos.hadzic@gmail.com>
2012-04-07 17:07:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSS Timer
+
+Simple javascript timer.
---
+++ timer.js
@@ -0,0 +1,65 @@
+var count;
+var timerId;
+
+init()
+
+function init()
+{
+  reset();
+  $('#pause').on('click', pause)
+  $('#start').on('click', start)
+  $('#reset').on('click', reset)
+}
+
+function timer()
+{
+  if (count <=0)
+    clearInterval(timerId);
+  else
+    count--;
+
+  displayTime(count);
+}
+
+function displayTime(seconds)
+{
+  var time = toMinutes(seconds);
+  $('#timer').html(time[0] + ':' + addZero(time[1]));
+}
+
+function toMinutes(seconds)
+{
+  var time = [];
+  time[0] = Math.floor(seconds/60)
+  time[1] = seconds - time[0] * 60
+  return time;
+}
+
+function addZero(num)
+{
+  return (String(num).length < 2) ? "0" + num :  num;
+}
+
+function pause()
+{
+  $('#pause').fadeOut(300, function () {
+    $('#start').fadeIn();
+  });
+
+  clearInterval(timerId);
+}
+
+function start()
+{
+  $('#start').fadeOut(300, function () {
+    $('#pause').fadeIn();
+  });
+
+  timerId = setInterval(timer, 1000);
+}
+
+function reset()
+{
+  count = 15 * 60;
+  displayTime(count);
+}
